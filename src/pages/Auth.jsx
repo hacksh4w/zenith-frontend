1211ebdde598a7ed3fbff8e083cd1ec89416bcd3
subru <easwaransubramaniOO7@gmail.com>
2023-09-23 11:40:33 +0530

    added context api and landing/auth page structure

---
+++ src/pages/Auth.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ContainerStyles } from '../../palette'
+
+const Auth = () => {
+  return (
+    <ContainerStyles>
+        
+    </ContainerStyles>
+  )
+}
+
+export default Auth
